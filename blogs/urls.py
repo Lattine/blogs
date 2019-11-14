7de8c1b62190fa16ed177b5116d7bc3d1a1b41b0
lattine <myaura@163.com>
2019-11-14 21:05:24 +0800

    update

--- blogs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("article/", include("article.urls", namespace="article")),
-    path("user/", include("user.urls", namespace="user")),
-]
