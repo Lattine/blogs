4d16a1e6ad6f24379b763e9045e59342cca0ecf2
Z <myaura@163.com>
2020-04-21 17:36:30 +0800

    update

--- backend/app/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("api", __name__)
-
-from app.api import ping, users, posts, tokens  # 写在Blueprint之后，是避免循环引用
--- backend/app/api/ping.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-from app.api import bp
-
-
-@bp.route("/ping", methods=["GET"])
-def ping():
-    return jsonify("Pong!")
